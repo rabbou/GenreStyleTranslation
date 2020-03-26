933c0b7cdbd16250640abfb6237405651b1af12d
Ruben Abbou <ruben@abbou.com>
2020-03-26 14:44:58 -0400

    adds

--- build_dataset.py
+++ build_dataset.py
@@ -1,3 +1,5 @@
+# kmeans clustering using PCA inspired by https://github.com/baolingfeng/PSC2CODE/blob/02e40d33ad4c9b92d7772977bad3ad78aec08966/python/baseline_scripts/clusterImagesPCA.py
+
 import argparse, json, csv, random, os, shutil, re, glob, sys
 import urllib.request as req
 import numpy as np
@@ -11,8 +13,24 @@ from sklearn.model_selection import train_test_split
 from collections import defaultdict
 from scipy import misc
 
+parser = argparse.ArgumentParser()
 parser.add_argument('--genres', nargs='+', default=['Metal', 'Dance', 'Classic'],
                     help='Genres as a list')
+parser.add_argument('--labels_path', default='albums/MuMu_dataset_multi-label.csv',
+                    help="File with MuMu album genres")
+
+def create_dir(dir_name):
+    """Create directory with check not to overwrite existing directory"""
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    else:
+        print("Warning: dir {} already exists".format(dir_name))
+
+def create_with_overwrite(dir_name):
+    """Create new directory or overwrite existing dir if it exists."""
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+    os.mkdir(dir_name)
 
 def has_face(image):
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -20,12 +38,11 @@ def has_face(image):
     faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30))
     return len(faces) > 0
 
-def make_data(labels):
-    paths = ['/Users/ruben/Box/CV Project/pytorch-CycleGAN-and-pix2pix/datasets/' + label + '/' for label in labels]
+def make_data(labels, labels_path):
+    paths = ['albums/' + label + '/' for label in labels]
     for path in paths:
-        if not os.path.exists(path):
-            os.mkdir(path)
-    with open('albums/MuMu_dataset_multi-label.csv', 'r', newline='') as csvf:
+        create_with_overwrite(path)
+    with open(labels_path, 'r', newline='') as csvf:
         readc = csv.reader(csvf, delimiter=',')
         for album in readc:
             path = "albums/MUMU/" + album[0] + ".jpg"
@@ -33,9 +50,83 @@ def make_data(labels):
                 img = cv2.imread(path)
                 if not has_face(img):
                     # leave out album covers with faces on them
-                    img = misc.imresize(image, (256,256))
+                    img = Image.fromarray(img).resize((256,256))
                     for i in range(len(labels)):
                         if labels[i] in album[5]:
-                            shutil.copy(img, paths[i] + album[0] + ".jpg")
+                            img.save(paths[i] + album[0] + ".jpg")
+
+def combine_images_into_tensor(img_fnames, size=256):
+    # Initialize the tensor
+    tensor = np.zeros((len(img_fnames), size * size))
+    for i, fname in enumerate(img_fnames):
+        img = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+        tensor[i] = img.reshape(size * size)
+    return tensor
+
+def get_pca_reducer_incremental(tr_tensor, n_comp=10, bs=10):
+    # Apply Incremental PCA on the training images
+    pca = IncrementalPCA(n_components=n_comp, batch_size=bs)
+    for i in range(0, len(tr_tensor), bs):
+        print(f"fitting {i//bs} th batch")
+        pca.partial_fit(tr_tensor[i:i+bs, :])
+    return pca
+
+def cluster_kmeans(all_img_fnames, num_clusters=4, bs=10):
+    # Select images at random for PCA
+    random.shuffle(all_img_fnames)
+    tr_img_fnames = all_img_fnames[:400]
+
+    # Flatten and combine the images
+    tr_tensor = combine_images_into_tensor(tr_img_fnames)
+
+    # Perform PCA
+    print("Learning PCA...")
+    n_comp = 10
+    pca = get_pca_reducer_incremental(tr_tensor, n_comp, bs)
+
+    # Transform images in batches
+    print("applying PCA transformation")
+    points = np.zeros((len(all_img_fnames), n_comp))
+    batch_size = 50
+    for i in range(0, len(all_img_fnames), batch_size):
+        print(f"Transforming {i//25} th batch")
+        batch_fnames = all_img_fnames[i:i+batch_size]
+        all_tensor = combine_images_into_tensor(batch_fnames)
+        points[i:i+batch_size] = pca.transform(all_tensor)
+
+    # Cluster
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(points)
+
+    # Organize image filenames based on the obtained clusters
+    cluster_fnames = defaultdict(list)
+    for i, label in enumerate(kmeans.labels_):
+        cluster_fnames[label].append(all_img_fnames[i])
+
+    return cluster_fnames
+
+def test_train_clustering(labels, method):
+    dirs = ['albums' + label + '/' for label in labels]
+    for i in range(len(labels)):
+        train_pth = 'albums/'+ labels[i] +'/train/'
+        test_pth = 'albums/'+ labels[i] +'/test/'
+        create_dir(train_pth)
+        create_dir(test_pth)
+        print(labels[i])
+        all_img_fnames = list(glob.glob(f"{dirs[i]}/*.jpg"))
+        if method == 'kmeans':
+            clustered_fnames = cluster_kmeans(all_img_fnames, num_clusters=5, bs=10)
+        elif method == 'em':
+            clustered_fnames = cluster_kmeans(all_img_fnames, num_clusters=5, bs=10)
+        mode = np.argmax([len(clustered_fnames[i]) for i in range(len(clustered_fnames))])
+        print('Cluster size:', len(clustered_fnames[mode]))
+        train, test = train_test_split(clustered_fnames[mode], test_size=0.2, random_state=42)
+        for album in train:
+            shutil.copy(album, train_pth)
+        for album in test:
+            shutil.copy(album, test_pth)
 
 if __name__ == '__main__':
+    args = parser.parse_args()
+    make_data(args.genres, args.labels_path)
+    test_train_clustering(['Metal', 'Dance', 'Classic'], cl='kmeans')
+    build_gan_ds('Metal', 'Classic', 'metal2classic')
--- download_data.py
+++ download_data.py
@@ -1,4 +1,4 @@
-# data scraping source: https://github.com/koenig125/album-artwork-classification
+# data scraping source: https://github.com/koenig125/album-artwork-classification/blob/master/build_dataset.py
 
 import argparse, json, csv, random, os, shutil, re, glob, sys
 import urllib.request as req
@@ -7,20 +7,12 @@ from PIL import Image
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import cv2
-from sklearn.decomposition import IncrementalPCA
-from sklearn.cluster import KMeans
-from sklearn.model_selection import train_test_split
-from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='albums/MUMU',
                     help="Directory with MUMU album images")
-parser.add_argument('--output_dir', default='albums/300x300_MUMU',
-                    help="Where to write the new data")
 parser.add_argument('--mumu_metadata', default='albums/amazon_metadata_MuMu.json',
                     help="File with MuMu metadata")
-parser.add_argument('--data_labels', default='albums/MuMu_dataset_multi-label.csv',
-                    help="File with MuMu album genres")
 
 def create_dir(dir_name):
     """Create directory with check not to overwrite existing directory"""
@@ -35,7 +27,6 @@ def create_with_overwrite(dir_name):
         shutil.rmtree(dir_name)
     os.mkdir(dir_name)
 
-
 def get_image_urls(mumu_metadata):
     """Retrieve all urls for album cover images in the MuMu dataset"""
     with open(mumu_metadata) as f:
@@ -47,7 +38,6 @@ def get_image_urls(mumu_metadata):
         img_urls[img_id] = img_url
     return img_urls
 
-
 def download_images(img_urls, data_dir):
     """Download album cover images from the MuMu dataset to `data_dir`"""
     create_dir(data_dir)
@@ -55,19 +45,6 @@ def download_images(img_urls, data_dir):
         img_format = img_url[-4:]
         img_file = os.path.join(data_dir, img_id + img_format)
         req.urlretrieve(img_url, img_file)
-
-
-def generate_splits(filenames):
-    """Generate 80/20 train/test splits for data"""
-    filenames.sort()
-    random.seed(230)
-    random.shuffle(filenames)
-    split_1 = int(0.8 * len(filenames))
-    filenames_train = filenames[:split_1]
-    filenames_test = filenames[split_1:]
-    splits = {'train': filenames_train, 'test': filenames_test}
-    return splits
-
 
 def get_album_genres(f_genres):
     """Retrieve genres for each album in the MuMu dataset"""
@@ -81,33 +58,11 @@ def get_album_genres(f_genres):
             album_genres[img_id] = genres
     return album_genres
 
-
-def generate_labels(filenames, album_genres, output_dir, split):
-    """Convert genres for each album to binary vector label and save to `output_dir`"""
-    genre_list = list(set([genre for genres in album_genres.values() for genre in genres]))
-    genre_list.sort()
-    labels = []
-    files = []
-    for f in filenames:
-        img_id = f.split('/')[-1][:-4]
-        genres = album_genres[img_id]
-        album_label = [1 if g in genres else 0 for g in genre_list]
-        if 1 not in album_label:
-            print(album_label)
-            print(genres)
-        labels.append(album_label)
-        files.append(f)
-    output_file = os.path.join(output_dir, 'y_' + split + '.npy')
-    np.save(output_file, np.array(labels))
-    return files
-
-
 def save(filenames, output_dir):
     """Save the images contained in `filenames` to the `output_dir`"""
     for filename in tqdm(filenames):
         image = Image.open(filename)
         image.save(os.path.join(output_dir, filename.split('/')[-1]))
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -119,28 +74,6 @@ if __name__ == '__main__':
         download_images(img_urls, args.data_dir)
 
     # Get the filenames in the data directory
-    filenames = [os.path.join(args.data_dir, f) for f in os.listdir(args.data_dir) if f.endswith('.jpg')]
-
-    # Split into 80%/20% train/test
-    splits = generate_splits(filenames)
-
-    # Create new output data directory
-    create_dir(args.output_dir)
-
-    # Preprocess train, dev and test images
-    album_genres = get_album_genres(args.data_labels)
-    for split, files in splits.items():
-        dir_split = os.path.join(args.output_dir, split)
-        create_with_overwrite(dir_split)
-        output_dir_images = os.path.join(dir_split, 'images')
-        create_with_overwrite(output_dir_images)
-        output_dir_genres = os.path.join(dir_split, 'genres')
-        create_with_overwrite(output_dir_genres)
-
-        print("Generating {} labels, saving to {}".format(split, output_dir_genres))
-        files_present = generate_labels(files, album_genres, output_dir_genres, split)
-
-        print("Processing {} data, saving to {}".format(split, output_dir_images))
-        save(files_present, output_dir_images)
+    # filenames = [os.path.join(args.data_dir, f) for f in os.listdir(args.data_dir) if f.endswith('.jpg')]
 
     print("Done building dataset")
